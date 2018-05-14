03191b27c0b25e6c351c24d8bf571c80a1b1cf13
Arvi Punzalan <arvi1222@yahoo.com>
2018-05-13 23:01:01 -0700

    add title to jobs model

--- jobs/models.py
+++ jobs/models.py
@@ -4,3 +4,4 @@ class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
     hyperlink = models.CharField(max_length=100, default='add hyperlink')
+    title = models.CharField(max_length=50, default='add title')
